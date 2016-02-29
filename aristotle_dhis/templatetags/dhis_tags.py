63418e9ffc0d68cf2f746c69d287a70c4054c7a6
Samuel Spencer <theodore.therone@gmail.com>
2016-02-29 06:20:03 +0000

    update tags

--- aristotle_dhis/templatetags/dhis_tags.py
+++ aristotle_dhis/templatetags/dhis_tags.py
@@ -5,3 +5,26 @@ register = template.Library()
 def public_flags(item):
 
     return "r-------"
+
+
+def value_type(item):
+
+    type_map = {
+        #"":"Code",
+        "Date/Time":"DATE",
+        #"":"ExternalCategory",
+        #"":"GeographicLocationCode",
+        #"":"GeographicStructureCode",
+        "Number":"NUMBER",
+        "Currency":"NUMBER",
+        #"":"Scale",
+        "String":"TEXT"
+    }
+    value_type = "TEXT"
+    num_values = item.permissiblevalue_set.all().count()
+    if item.data_type and num_values == 0:
+        value_type = type_map.get(item.data_type.name)
+    elif num_values > 0:
+        value_type = "TEXT"
+    
+    return value_type
